c2c9d532aa04075d585b8c81a3a544d99d555965
Chotika <chotika0447@gmail.com>
2026-07-27 12:00:05 +0700

    test commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import NewsCard from "./NewsCard"
+
+function Home() {
+
+    return (
+
+        <home>
+
+            <h2>ยินดีต้อนรับ</h2>
+
+            <p>นี่คือเว็บ React แรกของฉัน</p>
+            <NewsCard  />
+            <NewsCard  />
+            <NewsCard  />
+        </home>
+
+    )
+
+}
+
+export default Home
