17224e58c4ae6f6d134df5c393726204c88c6673
chotika0447 <chotika0447@gmail.com>
2026-08-20 15:55:34 +0700

    Add project backend and frontend

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Home from './FinaceHome'
+
+createRoot(document.getElementById('root')).render(
+  
+    <Home />
+  
+)
